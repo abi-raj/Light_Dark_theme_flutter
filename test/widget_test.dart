33a5b566db0369bff372193e2ee83f0a35fddd4d
abiraj-rajendran <abiraj.rajendran@zoho.com>
2021-09-16 10:03:07 +0530

    folder structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:light_dark_theme_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MainWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
